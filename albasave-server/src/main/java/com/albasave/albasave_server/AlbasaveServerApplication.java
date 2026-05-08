89c104323ce22f3fe51f6e9638800fb357ddb7c7
jung-jaeil <jji001216@naver.com>
2026-05-08 19:11:15 +0900

    Add: Spring Boot 백엔드 서버 코드 추가

---
+++ albasave-server/src/main/java/com/albasave/albasave_server/AlbasaveServerApplication.java
@@ -0,0 +1,13 @@
+package com.albasave.albasave_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbasaveServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbasaveServerApplication.class, args);
+	}
+
+}
---
+++ albasave-server/src/test/java/com/albasave/albasave_server/AlbasaveServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albasave.albasave_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbasaveServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
